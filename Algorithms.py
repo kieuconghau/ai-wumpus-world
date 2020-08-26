b0b8745c7f5006aa0ef6085f9ae325c7537f4e5f
oOo-Solar-oOo <50949696+oOo-Solar-oOo@users.noreply.github.com>
2020-08-26 18:21:01 +0700

    đọc file

--- Algorithms.py
+++ Algorithms.py
@@ -2,5 +2,18 @@
 
 class Algorithms:
     def __init__(self):
-        #read file
+        try:
+            self.file = open('input.txt', "r")
+        except:
+            print("Can not read file \'" + 'input.txt' + "\'. Please check again!")
+    def read_file(self):
+
+        self.size = [int(x) for x in next(self.file).split()]
+        self.raw_map = [[str(num) for num in line if num != '\n'] for line in self.file]
+    def from_raw_to_info(self):
         pass
+
+
+a = Algorithms()
+b, c = a.read_file()
+print(b, c)
